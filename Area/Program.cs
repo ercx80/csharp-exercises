88cfabc1835838d3b05439a2b872dc802c8c0d00
Erick Murrieta <ercx80@gmail.com>
2019-10-22 20:27:10 -0400

    Final Studio solution

---
+++ Area/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First I declare the variables
+            double area;
+            double radius;
+            double pi = 3.14;
+            string input;
+
+            
+                Console.WriteLine("Enter a radius: "); //This line asks user for input
+
+
+            
+
+                input = Console.ReadLine(); //This line takes the input from user
+            while (double.Parse(input) < 0)
+            {
+                Console.Write("You have to enter a positive number.!");
+                Console.ReadLine();
+                Console.WriteLine("Enter a radius: ");
+                input = Console.ReadLine();
+
+
+
+
+            }
+            if (double.Parse(input) > 0)
+
+            {
+                
+                
+                radius = double.Parse(input);//This line parses the input and stores it in the radius variable
+                area = (pi * radius * radius);//Calculation of the area is done here
+                Console.WriteLine("The area of a circle is: " + area);//This line represents the output
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+        }
+    }
+}
