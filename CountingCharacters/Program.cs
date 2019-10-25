f04b4bf1e568ec32520af102d64743bca0a20268
Erick Murrieta <ercx80@gmail.com>
2019-10-24 23:55:47 -0400

    Studio 2

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+             string myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc accumsan sem ut ligula scelerisque sollicitudin.Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat.Donec sit amet suscipit metus, non lobortis massa.Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+             Dictionary<char, int> myDict = new Dictionary<char, int>();
+             //int count = 0;
+
+
+
+
+            foreach (char c in myString)
+            {
+
+
+                if (myDict.ContainsKey(c))
+                {
+
+                    myDict[c]++;
+
+                }
+
+
+
+
+                else
+                {
+                    myDict.Add(c, 1);
+                }
+
+
+
+                Console.WriteLine($"{c}:{myDict[c]}");
+
+
+
+
+            }
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
+
+
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            //string myString = "My \"so called\" life";
+            //string myString = "need a\nnew line";
+            //string myString = "Go to c:\\ drive";
+            //string myString = @"Go to c:\ drive";
+
+            //string myString = String.Format("{1}={0}", "first", "second");
+            //Console.WriteLine(myString);
+            //Console.ReadLine();
+
+           /* List<int> myList = new List<int>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                myList.Add(i);
+                Console.WriteLine(myList);
+                Console.ReadLine();
+
+            }*/
+            var myArray = new Int32[1, 1, 2, 3, 5, 8];
+
+
+
 
         }
     }
