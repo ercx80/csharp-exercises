dda69f37333c3cf5da8f869b1a6b48d259c94378
Erick Murrieta <ercx80@gmail.com>
2019-11-19 19:40:45 -0500

    test

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -11,19 +11,23 @@ namespace CountingCharacters
         {
              string myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc accumsan sem ut ligula scelerisque sollicitudin.Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat.Donec sit amet suscipit metus, non lobortis massa.Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
              Dictionary<char, int> myDict = new Dictionary<char, int>();
-             //int count = 0;
+           
 
 
 
 
             foreach (char c in myString)
             {
-
+                
 
                 if (myDict.ContainsKey(c))
                 {
 
-                    myDict[c]++;
+                   // my logic here is if the letter is in the dictionary, add the letter +1.
+                    
+                     myDict[c]++;
+                    //myDict.Add(c, (myDict[c]));
+                   
 
                 }
 
@@ -32,6 +36,7 @@ namespace CountingCharacters
 
                 else
                 {
+                    //here if the letter is not then just add 1
                     myDict.Add(c, 1);
                 }
 
